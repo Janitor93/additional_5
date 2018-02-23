e022e0b65d4ce6e1a29ce81f030c9560359a3cac
Ruslan Prnkovskij <ruslan.penkovskij@gmail.com>
2018-02-23 18:15:03 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = {};
+  for(let i in bracketsConfig) {
+    brackets[bracketsConfig[i][1]] = bracketsConfig[i].join('');
+  }
+
+  let dublicate = str;
+  for(let i = 0; i < str.length; i++) {
+    if(brackets[str[i]] !== undefined) {
+      dublicate = dublicate.replace(brackets[str[i]], '');
+    }
+  }
+
+  return dublicate.length !== 0 ? false : true;
 }
